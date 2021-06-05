1dcd5f14b69cea48d72149ff7cf4b20f5b79d849
Siegfred Marie Jason "SJ" G. Lauron <siegfredlauron030794@gmail.com>
2021-06-05 15:21:05 +0800

    Edit-Details-Delete

--- WarehouseApp/Controllers/ProductController.cs
+++ WarehouseApp/Controllers/ProductController.cs
@@ -29,5 +29,78 @@ namespace WarehouseApp.Controllers
             }
             return View(product);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            WarehouseAppContext context = HttpContext.RequestServices.GetService(typeof(WarehouseAppContext)) as WarehouseAppContext;
+            Product product = context.GetProduct(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([Bind] Product product)
+        {
+            WarehouseAppContext context = HttpContext.RequestServices.GetService(typeof(WarehouseAppContext)) as WarehouseAppContext;
+            if(ModelState.IsValid){
+                context.UpdateProduct(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+             if (id == null)
+            {
+                return NotFound();
+            }
+            WarehouseAppContext context = HttpContext.RequestServices.GetService(typeof(WarehouseAppContext)) as WarehouseAppContext;
+            Product product = context.GetProduct(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+             if (id == null)
+            {
+                return NotFound();
+            }
+            WarehouseAppContext context = HttpContext.RequestServices.GetService(typeof(WarehouseAppContext)) as WarehouseAppContext;
+            Product product = context.GetProduct(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int? id)
+        {
+            WarehouseAppContext context = HttpContext.RequestServices.GetService(typeof(WarehouseAppContext)) as WarehouseAppContext;
+            context.DeleteProduct(id);
+            return RedirectToAction("Index");
+        }
     }
 }
--- WarehouseApp/WarehouseContext.cs
+++ WarehouseApp/WarehouseContext.cs
@@ -18,46 +18,104 @@ namespace WarehouseApp
             return new MySqlConnection(ConnectionString);
         }
 
+#region Product
         // Get all Product
         public List<Product> GetAllProducts()
         {
-            List<Product> list = new List<Product>();
+            List<Product> lstProduct = new List<Product>();
 
             using (MySqlConnection conn = GetConnection())
-            {
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand("select * from Product;", conn);
+            {             
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Product;", conn);
 
-                using (var reader = cmd.ExecuteReader())
+                conn.Open();
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
                 {
-                    while (reader.Read())
-                    {
-                        list.Add(new Product()
-                        {
-                            Id = Convert.ToInt32(reader["Id"]),
-                            Name = reader["Name"].ToString(),
-                            Quantity = Convert.ToInt32(reader["Quantity"])
-                        });
-                    }
+                    Product product = new Product();
+
+                    product.Id = Convert.ToInt32(rdr["Id"]);
+                    product.Name = rdr["Name"].ToString();
+                    product.Quantity = Convert.ToInt32(rdr["Quantity"]);
+                    
+                    lstProduct.Add(product);
                 }
                 conn.Close();
             }
-            return list;
-        }  
+            return lstProduct;
+        }
 
          //Add Product
          public void CreateProduct(Product product)
          {
              using(MySqlConnection conn = GetConnection())
              {
-                 conn.Open();
-                 MySqlCommand cmd = conn.CreateCommand();
-                 cmd.CommandText = "INSERT INTO Product(Name, Quantity) VALUES (@Name, @Quantity)";
+                 MySqlCommand cmd = new MySqlCommand("INSERT INTO Product(Name, Quantity) VALUES (@Name, @Quantity)", conn);
+
                  cmd.Parameters.AddWithValue("@Name", product.Name);
                  cmd.Parameters.AddWithValue("@Quantity", product.Quantity);
+
+                 conn.Open();
                  cmd.ExecuteNonQuery();
                  conn.Close();
              }
          }
+
+         //Edit/ Update Product
+         public void UpdateProduct(Product product)
+         {
+             using(MySqlConnection conn = GetConnection())
+             {
+                 MySqlCommand cmd = new MySqlCommand("UPDATE Product SET Name = @Name, Quantity = @Quantity WHERE Id = @Id", conn);
+
+                 cmd.Parameters.AddWithValue("@Id", product.Id);
+                 cmd.Parameters.AddWithValue("@Name", product.Name);
+                 cmd.Parameters.AddWithValue("@Quantity", product.Quantity);
+
+                 conn.Open();
+                 cmd.ExecuteNonQuery();
+                 conn.Close();
+             }
+         }
+
+         //Get the details of a particular Product
+        public Product GetProduct(int? id)
+        {
+            Product product = new Product();
+
+            using (MySqlConnection conn = GetConnection())
+            {             
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Product WHERE Id = @Id;", conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                conn.Open();
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    product.Id = Convert.ToInt32(rdr["Id"]);
+                    product.Name = rdr["Name"].ToString();
+                    product.Quantity = Convert.ToInt32(rdr["Quantity"]);
+                }
+                conn.Close();
+            }
+            return product;
+        }
+
+        //Delete the record on particular Product
+        public void DeleteProduct(int? id)
+        {
+             using (MySqlConnection conn = GetConnection())
+            {             
+                MySqlCommand cmd = new MySqlCommand("DELETE FROM Product WHERE Id = @Id;", conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }   
+        }
     }
+#endregion
 }
